5e6af60e1c92495da3d5891fb3c036a5f2137ec9
onrcn <onrcnernl@gmail.com>
2024-10-31 15:22:18 +0300

    compiler and tabstop

--- nvim/.config/nvim/lua/confy/core/remap.lua
+++ nvim/.config/nvim/lua/confy/core/remap.lua
@@ -1,16 +1,32 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
--- greatest remap ever
+
 vim.keymap.set("x", "<leader>p", [["_dP]])
--- next greatest remap ever : asbjornHaland
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+vim.keymap.set("n", "<leader>pf", vim.cmd.Ex)
+
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
+
+
+-- Open compiler
+vim.api.nvim_set_keymap('n', '<F6>', "<cmd>CompilerOpen<cr>", { noremap = true, silent = true })
+
+-- Redo last selected option
+vim.api.nvim_set_keymap('n', '<S-F6>',
+"<cmd>CompilerStop<cr>" -- (Optional, to dispose all tasks before redo)
+.. "<cmd>CompilerRedo<cr>",
+{ noremap = true, silent = true })
+
+-- Toggle compiler results
+vim.api.nvim_set_keymap('n', '<S-F7>', "<cmd>CompilerToggleResults<cr>", { noremap = true, silent = true })
