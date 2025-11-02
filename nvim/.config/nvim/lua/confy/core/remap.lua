eb8305f2482765fa889a83b8eb1b22b670ddfb0b
onrcn <onrcnernl@gmail.com>
2025-11-02 18:27:44 +0300

    sacma sapan bi error duzelttik xd

--- nvim/.config/nvim/lua/confy/core/remap.lua
+++ nvim/.config/nvim/lua/confy/core/remap.lua
@@ -1,9 +1,9 @@
 vim.g.mapleader = ' '
 
-vim.keymap.set('x', '<leader>p', [['_dP]])
-vim.keymap.set({ 'n', 'v' }, '<leader>y', [['+y]])
-vim.keymap.set('n', '<leader>Y', [['+Y]])
-vim.keymap.set({ 'n', 'v' }, '<leader>d', [['_d]])
+vim.keymap.set('x', '<leader>p', [["_dP]])
+vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]])
+vim.keymap.set('n', '<leader>Y', [["+Y]])
+vim.keymap.set({ 'n', 'v' }, '<leader>d', [["_d]])
 
 vim.keymap.set('n', '<leader>f', vim.lsp.buf.format)
 
