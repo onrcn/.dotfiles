75a49950dcce065d6f8e0a04fe71ab85ada8e35a
onrcn <onrcnernl@gmail.com>
2025-11-04 17:44:47 +0300

    hyprland changes

--- nvim/.config/nvim/lua/confy/plugins/lsp.lua
+++ nvim/.config/nvim/lua/confy/plugins/lsp.lua
@@ -133,22 +133,4 @@ return {
       },
     },
   },
-  { -- optional blink completion source for require statements and module annotations
-    "saghen/blink.cmp",
-    build = 'cargo +nightly build --release',
-    opts = {
-      sources = {
-        -- add lazydev to your completion providers
-        default = { "lazydev", "lsp", "path", "snippets", "buffer" },
-        providers = {
-          lazydev = {
-            name = "LazyDev",
-            module = "lazydev.integrations.blink",
-            -- make lazydev completions top priority (see `:h blink.cmp`)
-            score_offset = 100,
-          },
-        },
-      },
-    },
-  }
 }
