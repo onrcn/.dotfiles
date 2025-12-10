27295eed097ae32fa3edacf11967f85fb9533591
onrcn <onrcnernl@gmail.com>
2025-12-11 00:44:17 +0300

    (remap.lua) : Add standard LSP keymaps (gd, gD, etc.)

--- nvim/.config/nvim/lua/confy/config/remap.lua
+++ nvim/.config/nvim/lua/confy/config/remap.lua
@@ -9,6 +9,13 @@ vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, { desc = 'Format code' })
 vim.keymap.set({ 'n', 'i' }, '<F2>', vim.lsp.buf.rename, { desc = 'Rename' })
 vim.keymap.set({ 'n', 'v', 'i' }, '<C-/>', vim.lsp.buf.code_action, { silent = true, noremap = true, desc = 'Code action' })
 
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = 'Go to definition' })
+vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { desc = 'Go to declaration' })
+vim.keymap.set('n', 'gr', vim.lsp.buf.references, { desc = 'Go to references' })
+vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { desc = 'Go to implementation' })
+vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = 'Show signature' })
+
+
 vim.keymap.set('n', '<C-k>', '<cmd>cnext<CR>zz', { desc = 'Next in quickfix list' })
 vim.keymap.set('n', '<C-j>', '<cmd>cprev<CR>zz', { desc = 'Previous in quickfix list' })
 vim.keymap.set('n', '<leader>k', '<cmd>lnext<CR>zz', { desc = 'Next in location list' })
