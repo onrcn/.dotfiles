1226e48dd7ab3b52587be2c8c8006af0b358c3ae
onrcn <onrcnernl@gmail.com>
2025-12-10 23:26:48 +0300

    (init.lua): remove fine-cmdline plugin

--- nvim/.config/nvim/lua/confy/plugins/init.lua
+++ nvim/.config/nvim/lua/confy/plugins/init.lua
@@ -1,13 +1,5 @@
 return {
   {
-    'VonHeikemen/fine-cmdline.nvim',
-    lazy = false,
-    dependencies = { 'MunifTanjim/nui.nvim' },
-    config = function()
-      vim.api.nvim_set_keymap('n', ':', '<cmd>FineCmdline<CR>', { noremap = true })
-    end,
-  },
-  {
     'tpope/vim-fugitive',
     config = function()
       vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
