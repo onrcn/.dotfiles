cc50cc243590dff54f9ace9e14da25bb0977b96f
onrcn <onrcnernl@gmail.com>
2025-12-11 00:30:28 +0300

    (whichkey.lua) : Add 1-second delay to which-key execution

--- nvim/.config/nvim/lua/confy/plugins/whichkey.lua
+++ nvim/.config/nvim/lua/confy/plugins/whichkey.lua
@@ -2,6 +2,8 @@ return {
   'folke/which-key.nvim',
   event = 'VimEnter',
   config = function()
-    require('which-key').setup({})
+    require('which-key').setup({
+      delay = 1000,
+    })
   end,
 }
