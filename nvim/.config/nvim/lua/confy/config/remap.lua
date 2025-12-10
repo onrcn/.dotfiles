cffb6d37a0bcd88908e8e2012cc8b32a452ef4b3
onrcn <onrcnernl@gmail.com>
2025-12-10 23:07:54 +0300

    feat: setup which-key

--- nvim/.config/nvim/lua/confy/config/remap.lua
+++ nvim/.config/nvim/lua/confy/config/remap.lua
@@ -1,37 +1,37 @@
 vim.g.mapleader = ' '
 
-vim.keymap.set('x', '<leader>p', [["_dP]])
-vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]])
-vim.keymap.set('n', '<leader>Y', [["+Y]])
-vim.keymap.set({ 'n', 'v' }, '<leader>d', [["_d]])
+vim.keymap.set('x', '<leader>p', [["_dP]], { desc = 'Paste without losing yanked text' })
+vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]], { desc = 'Yank to system clipboard' })
+vim.keymap.set('n', '<leader>Y', [["+Y]], { desc = 'Yank line to system clipboard' })
+vim.keymap.set({ 'n', 'v' }, '<leader>d', [["_d]], { desc = 'Delete without yanking' })
 
-vim.keymap.set('n', '<leader>f', vim.lsp.buf.format)
-vim.keymap.set({ 'n', 'i' }, '<F2>', vim.lsp.buf.rename)
-vim.keymap.set({ 'n', 'v', 'i' }, '<C-/>', vim.lsp.buf.code_action, { silent = true, noremap = true } )
+vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, { desc = 'Format code' })
+vim.keymap.set({ 'n', 'i' }, '<F2>', vim.lsp.buf.rename, { desc = 'Rename' })
+vim.keymap.set({ 'n', 'v', 'i' }, '<C-/>', vim.lsp.buf.code_action, { silent = true, noremap = true, desc = 'Code action' })
 
-vim.keymap.set('n', '<C-k>', '<cmd>cnext<CR>zz')
-vim.keymap.set('n', '<C-j>', '<cmd>cprev<CR>zz')
-vim.keymap.set('n', '<leader>k', '<cmd>lnext<CR>zz')
-vim.keymap.set('n', '<leader>j', '<cmd>lprev<CR>zz')
+vim.keymap.set('n', '<C-k>', '<cmd>cnext<CR>zz', { desc = 'Next in quickfix list' })
+vim.keymap.set('n', '<C-j>', '<cmd>cprev<CR>zz', { desc = 'Previous in quickfix list' })
+vim.keymap.set('n', '<leader>k', '<cmd>lnext<CR>zz', { desc = 'Next in location list' })
+vim.keymap.set('n', '<leader>j', '<cmd>lprev<CR>zz', { desc = 'Previous in location list' })
 
-vim.keymap.set('n', '<leader>pf', vim.cmd.Ex)
+vim.keymap.set('n', '<leader>pf', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
 
 vim.keymap.set('n', '<leader><leader>', function()
   vim.cmd('so')
-end)
+end, { desc = 'Source current file' })
 
-local opts = { noremap = true, silent = true, desc = 'Diagnostics' }
+vim.keymap.set('n', '<leader>?', '<cmd>WhichKey<CR>', { desc = 'Open WhichKey' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
 
 -- Open compiler
-vim.api.nvim_set_keymap('n', '<F6>', '<cmd>CompilerOpen<cr>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<S-F6>', '<cmd>CompilerOpen<cr>', { noremap = true, silent = true, desc = 'Open compiler' })
 
 -- Redo last selected option
-vim.api.nvim_set_keymap('n', '<S-F6>',
+vim.api.nvim_set_keymap('n', '<F6>',
   '<cmd>CompilerStop<cr>' -- (Optional, to dispose all tasks before redo)
   .. '<cmd>CompilerRedo<cr>',
-  { noremap = true, silent = true })
+  { noremap = true, silent = true, desc = 'Redo last compiler option' })
 
 -- Toggle compiler results
-vim.api.nvim_set_keymap('n', '<S-F7>', '<cmd>CompilerToggleResults<cr>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<S-F7>', '<cmd>CompilerToggleResults<cr>', { noremap = true, silent = true, desc = 'Toggle compiler results' })
