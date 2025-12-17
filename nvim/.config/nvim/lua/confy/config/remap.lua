83675593889dd6752a1dd946cc2aa08df4b7d208
onrcn <onrcnernl@gmail.com>
2025-12-17 20:14:05 +0300

    feat(tabs): replace barbar with native tabs and add keymaps

--- nvim/.config/nvim/lua/confy/config/remap.lua
+++ nvim/.config/nvim/lua/confy/config/remap.lua
@@ -4,3 +4,8 @@ vim.keymap.set('x', '<leader>p', [["_dP]], { desc = 'Paste without losing yanked
 vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]], { desc = 'Yank to system clipboard' })
 vim.keymap.set('n', '<leader>Y', [["+Y]], { desc = 'Yank line to system clipboard' })
 vim.keymap.set({ 'n', 'v' }, '<leader>d', [["_d]], { desc = 'Delete without yanking' })
+
+for i = 1, 9 do
+  vim.keymap.set('n', string.format('<A-%d>', i), string.format('%dgt', i), { desc = string.format('Go to tab %d', i) })
+end
+
--- nvim/.config/nvim/lua/confy/config/set.lua
+++ nvim/.config/nvim/lua/confy/config/set.lua
@@ -7,6 +7,7 @@ vim.opt.expandtab = true
 
 vim.opt.smartindent = true
 vim.opt.wrap = false
+vim.opt.showtabline = 2
 vim.opt.swapfile = false
 vim.opt.backup = false
 
